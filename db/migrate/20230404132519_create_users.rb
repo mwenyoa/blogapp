0270cbc28af795c390901d17d68282aa31763068
Anthony Mwenyo <mwenyoanthony@gmail.com>
2023-05-09 12:47:05 +0200

    Generate the users moddel

---
+++ db/migrate/20230404132519_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :firstname
+      t.string :lastname
+      t.string :email
+      t.string :bio
+      t.integer :posts_counter, default: 0
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
